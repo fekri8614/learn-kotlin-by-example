eeb7e89909f0a7e70e41970b437b8301aca50131
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-05-22 01:47:25 -0700

    Productivity Boosters

---
+++ src/main/kotlin/productivityBoosters/DescurtingDeclarations2.kt
@@ -0,0 +1,16 @@
+package productivityBoosters
+
+/**
+ * [Productivity Boosters ( >Destructuring Declarations)](https://play.kotlinlang.org/byExample/08_productivity_boosters/03_Destructuring%20Declarations)
+ * */
+
+// third
+private class Pair<K, V>(val first: K, val second: V) {
+    operator fun component1(): K = first
+    operator fun component2(): V = second
+}
+
+fun main() {
+    val (num, name) = Pair(1, "one")
+    println("num = $num , name = $name")
+}
