5aedfeb37365c7437cd0f1c3445ad12357cca6e4
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-06-21 04:06:26 -0700

    `Generic2.kt` is a sample of Generic functions and classes.

--- src/main/kotlin/kotlinlang/learn_pack2/Generic2.kt
+++ src/main/kotlin/kotlinlang/learn_pack2/Generic2.kt
@@ -4,26 +4,26 @@ abstract class SomeClasses<T> {
     abstract fun execute(): T
 }
 
-class SomeImplementation: SomeClasses<String>() {
+class SomeImplementation : SomeClasses<String>() {
     override fun execute(): String = "Test"
 }
 
-class OtherImplementation: SomeClasses<Int>() {
+class OtherImplementation : SomeClasses<Int>() {
     override fun execute(): Int = 42
 }
 
 object Runner {
-    inline fun <reified S: SomeClasses<T>, T> run(): T {
+    inline fun <reified S : SomeClasses<T>, T> run(): T {
         return S::class.java.getDeclaredConstructor().newInstance().execute()
     }
 }
 
 fun main() {
 
-    val s= Runner.run<SomeImplementation, _>()
+    val s = Runner.run<SomeImplementation, _>()
     assert(s == "Test")
 
     val n = Runner.run<OtherImplementation, _>()
-    assert(n == 22)
+    assert(n == 42)
 
 }
