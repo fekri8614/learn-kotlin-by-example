4bfd3acf47075a3c137ad17f6d74988e9ebb1370
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-02 10:57:40 -0700

    worked with `Flow cancellation basics`

--- src/main/kotlin/kotlinlang/learn_pack2/flow/KotlinFlowSamp.kt
+++ src/main/kotlin/kotlinlang/learn_pack2/flow/KotlinFlowSamp.kt
@@ -7,16 +7,15 @@ fun simple(): Flow<Int> = flow {
     println("Flow started")
     for (i in 1..3) {
         delay(100)
+        println("Emitting $i")
         emit(i)
     }
 }
 
 private fun main() = runBlocking {
-    println("Calling simple function...")
-    val flow = simple()
-    println("Calling collect...")
-    flow.collect { value -> println(value) }
-    println("Calling collect again...")
-    flow.collect { value -> println(value) }
+    withTimeoutOrNull(250) {
+        simple().collect { value -> println(value) }
+    }
+    println("Done!")
 }
 
