cadd68722e35b54ab97704d4029f007ad1627246
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-08-02 12:57:07 -0700

    worked with `Terminal flow operators`

--- src/main/kotlin/kotlinlang/learn_pack2/flow/KotlinFlowSamp.kt
+++ src/main/kotlin/kotlinlang/learn_pack2/flow/KotlinFlowSamp.kt
@@ -3,22 +3,10 @@ package kotlinlang.learn_pack2.flow
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
-fun numbers(): Flow<Int> = flow {
-    try {
-        emit(1)
-        emit(2)
-        println("this line will not execute")
-        emit(3)
-    }catch (e: Exception) {
-        println("ERROR:--> $e")
-    } finally {
-        println("Finally in numbers")
-    }
-}
-
 private fun main() = runBlocking {
-    numbers()
-        .take(2)
-        .collect { value -> println(value) }
+    val sum = (1..5).asFlow()
+        .map { it * it }
+        .reduce { a, b -> a + b }
+    println(sum)
 }
 
